548cbb443517dc9216196958655f336bfa10c529
huy2122k <wwwhuy212@gmail.com>
2022-07-02 18:57:23 +0700

    fix bugs check_duplicate

--- utils/deduplicate_mongo.py
+++ utils/deduplicate_mongo.py
@@ -8,17 +8,32 @@ from pandas.api.types import (is_datetime64_dtype, is_numeric_dtype,
 from pymongo import MongoClient
 from recordlinkage.index import Full
 
+from utils.constants import DataType
+from utils.context import Context
+
+dict_convert = {
+    DataType.INT : pd.to_numeric, 
+    DataType.DATE : pd.to_datetime,
+    DataType.FLOAT : pd.to_numeric,
+}
 
 def find_duplicate(collection): 
     data = pd.DataFrame(list(collection.find()))
     data.set_index("_id",drop = True, inplace = True)
-    print(data.info())
-    print(len(data.columns))
-    print(data.columns[0])
+    schema = Context.project["destination schema"]
+    for column in schema.keys():
+        if schema[column] in dict_convert:
+            data[column] = dict_convert[schema[column]](data[column], errors='coerce')
+        else:
+            data[column] = data[column].astype("string", errors='ignore')
+    data = data.dropna()
 
     print("==============================")
+    print(data.info())
+
     if len(data.columns) < 1 or collection.count_documents({})<2:
         return []
+
     # Indexation step
     indexer = recordlinkage.Index()
     if collection.count_documents({}) > 70:
@@ -31,24 +46,27 @@ def find_duplicate(collection):
     print("Compare amount: "+ str(len(candidate_links)))
 
     compare_cl = recordlinkage.Compare()
-    string_compare_methods = ['jarowinkler','levenshtein', 'damerau_levenshtein', 'qgram', 'cosine']
+    string_compare_methods = [('jaro', 0.75),('jarowinkler',0.75), ('levenshtein',0.65), ('damerau_levenshtein',0.8), ('qgram',0.85),('cosine',0.85), ('smith_waterman',0.85), ('lcs',0.85)]
 
     for label in data.columns:
         if is_string_dtype(data[label]):
-            compare_cl.string(label, label, method=random.choice(string_compare_methods) , threshold=0.85)
+            method, ths = random.choice(string_compare_methods)
+            compare_cl.string(label, label, method= method, threshold=ths)
         elif is_numeric_dtype(data[label]):
             compare_cl.numeric(label, label)
         elif is_datetime64_dtype(data[label]):
             compare_cl.date(label, label)
 
-
     # Comparison step
 
     features = compare_cl.compute(candidate_links, data)
+    features.to_csv("compare.csv")
     upper_bound = (len(data.columns)*3) //4 if len(data.columns) > 2 else len(data.columns)  
     # Classification step
     matches = features[features.sum(axis=1) >= upper_bound]
-    print(matches)
+    print("Total duplicate: " +str(len(matches)))
+    if len(matches) == 0:
+        return []
     ids_1,ids_2 = map(list,zip(*list(matches.index)))
-    return ids_2
+    return list(dict.fromkeys(ids_2))
 
